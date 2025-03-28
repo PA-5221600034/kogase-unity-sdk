cedb36acd4bf742db71f8b7a268e7a26b0a50745
Atqa Munzir <atqamz@gmail.com>
2025-03-28 11:55:23 +0700

    feat: Cache

---
+++ Runtime/Core/Cache/FileCacheImpl.cs
@@ -0,0 +1,141 @@
+﻿#if !UNITY_WEBGL
+using System.Text;
+#endif
+
+namespace Kogase.Core
+{
+    internal class FileCacheImpl : ICacheImpl<string>
+    {
+        const int ReadWriteAsyncWaitMs = 100;
+        
+        readonly string cacheDirectory;
+
+        readonly IFileStream fs;
+        readonly IDebugger logger;
+
+        public FileCacheImpl(string cacheDirectory, IFileStream fs = null, IDebugger logger = null)
+        {
+            if (fs == null) fs = KogaseSDK.Implementation.FileStream;
+
+            if (string.IsNullOrEmpty(cacheDirectory))
+                throw new System.InvalidOperationException("Cache directory is empty.");
+            this.cacheDirectory = cacheDirectory;
+            this.fs = fs;
+            this.logger = logger;
+        }
+
+        public bool Contains(string key)
+        {
+            var itemPath = GetFileFullPath(key);
+            var retval = fs.IsFileExist(itemPath);
+            return retval;
+        }
+
+        public virtual bool Emplace(string key, string item)
+        {
+            try
+            {
+                var itemSavePath = GetFileFullPath(key);
+                fs.WriteFile(null, item, itemSavePath, null, true);
+            }
+            catch (System.Exception ex)
+            {
+                logger?.LogWarning(ex.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        public virtual async void EmplaceAsync(string key, string item, System.Action<bool> callback = null)
+        {
+            var filePath = GetFileFullPath(key);
+            var writeSuccess = false;
+            while (!writeSuccess)
+                try
+                {
+                    fs.WriteFileAsync(item, filePath, (success) => { writeSuccess = success; });
+                }
+                catch (System.Exception)
+                {
+                    await System.Threading.Tasks.Task.Delay(ReadWriteAsyncWaitMs);
+                }
+
+            callback?.Invoke(true);
+        }
+
+        public bool Update(string key, string item)
+        {
+            if (!Contains(key)) return false;
+            return Emplace(key, item);
+        }
+
+        public void Empty()
+        {
+            try
+            {
+                fs.DeleteDirectory(cacheDirectory, null);
+            }
+            catch (System.Exception exception)
+            {
+                logger?.LogWarning($"Failed to delete cache directory: {cacheDirectory}.\n{exception.Message}");
+            }
+        }
+
+        public virtual string Retrieve(string key)
+        {
+            var filePath = GetFileFullPath(key);
+            string retval = null;
+
+            try
+            {
+                fs.ReadFile(null, filePath, instantRead: true, onDone: (isSucess, readResult) =>
+                {
+                    if (isSucess) retval = (string)readResult;
+                });
+            }
+            catch (System.Exception ex)
+            {
+                logger?.LogWarning(ex.Message);
+            }
+
+            return retval;
+        }
+
+        public virtual void RetrieveAsync(string key, System.Action<string> callback)
+        {
+            if (callback != null)
+            {
+                var filePath = GetFileFullPath(key);
+                fs.ReadFileAsync(filePath, (success, readResult) => { callback?.Invoke(readResult); });
+            }
+        }
+
+        public string Peek(string key)
+        {
+            return Retrieve(key);
+        }
+
+        public bool Remove(string key)
+        {
+            var result = false;
+            try
+            {
+                var filePath = GetFileFullPath(key);
+                fs.DeleteFile(filePath, instantDelete: true, onDone: (isSuccess) => { result = isSuccess; });
+            }
+            catch (System.Exception ex)
+            {
+                logger?.LogWarning($"Failed to delete cache file.\n{ex.Message}");
+            }
+
+            return result;
+        }
+
+        string GetFileFullPath(string key)
+        {
+            var retval = $"{cacheDirectory}/{key}";
+            return retval;
+        }
+    }
+}
---
+++ Runtime/Core/Cache/ICacheImpl.cs
@@ -0,0 +1,59 @@
+﻿namespace Kogase.Core
+{
+    internal interface ICacheImpl<TFileType>
+    {
+        /**
+	    * @brief Check if the class stores an item with specified key
+	    *
+	    * @param key Identifier of the data
+	    * @return True if the key is found
+	    */
+        bool Contains(string key);
+
+        /**
+        * @brief Cleanup the storage
+        */
+        void Empty();
+
+        /**
+        * @brief Remove a data from the class
+        *
+        * @param key Identifier of the data
+        * @return True if the deleted data exist
+        * @return True if the removal success
+        */
+        bool Remove(string key);
+
+        /**
+        * @brief Write/replace an item with specified key
+        *
+        * @param key Identifier of the data
+        * @return True if the key is found
+        */
+        bool Emplace(string key, TFileType item);
+
+        /**
+        * @brief Update an item with specified key
+        *
+        * @param key Identifier of the data
+        * @return True if the key is found
+        */
+        bool Update(string key, TFileType item);
+
+        /**
+        * @brief Obtain and access the data
+        *
+        * @param key Identifier of the data
+        * @return Reference to the data.
+        */
+        TFileType Retrieve(string key);
+
+        /**
+        * @brief Obtain the data, only for update the data. Used for some cases like LRU algorithm.
+        *
+        * @param key Identifier of the data
+        * @return Reference to the data.
+        */
+        TFileType Peek(string key);
+    }
+}
