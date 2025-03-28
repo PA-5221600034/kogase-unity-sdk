907dd9df19d6bcb52143716fccd191ac3b975884
Atqa Munzir <atqamz@gmail.com>
2025-03-28 11:56:15 +0700

    feat: Boostrap

---
+++ Runtime/Bootstrap/SdkInterfaceBootstrap.cs
@@ -0,0 +1,14 @@
+﻿namespace Kogase.Bootstrap
+{
+    internal static class SdkInterfaceBootstrap
+    {
+        internal static void Execute()
+        {
+        }
+
+        public static void Stop()
+        {
+            KogaseSDK.Implementation.Reset();
+        }
+    }
+}
